1f81d903eeeafdd3762105dea0c96685efa77732
barrdeepak <dpak.nsit@gmail.com>
2023-03-08 00:40:09 +0800

    First commit for flutter app

---
+++ android/app/src/main/kotlin/come/dbarr/captions/MainActivity.kt
@@ -0,0 +1,6 @@
+package come.dbarr.captions
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data.dart
@@ -0,0 +1,11 @@
+
+class CaptionData {
+
+  Map getData() {
+    var map = new Map();
+    map["sun"] = "Sunsets are best";
+    map["sunset"] = "Sunsets are best";
+    map["coffee"] = "I am yet to wake up. Coffee please.";
+    return map;
+  }
+}
